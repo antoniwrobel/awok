6793564e872300195fd1414187ee8c43ae1b9302
antoni.wrobel <antoni.wrobel@imaginecurve.com>
2023-02-27 11:26:40 +0100

    scss refactor

--- src/index.tsx
+++ src/index.tsx
@@ -7,20 +7,22 @@ import ReactDOM from "react-dom/client";
 // Local imports
 import App from "./app";
 import initI18n from "./lang";
-import styles from "./index.module.scss";
+import styles from "../src/index.module.scss";
 import Layout from "./components/Layout";
 import AuthProvider from "./context/AuthProvider";
 import ErrorBoundary from "./error-boundary";
 import LoadingProvider from "./context/LoadingProvider";
 
+import reportWebVitals from "./util/web-vitals";
 import "./styles/main.scss";
-// import reportWebVitals from "./util/web-vitals";
 
 // Global initialization
 initI18n();
 
 const htmlRoot = document.getElementById("root") as HTMLElement;
 const reactRoot = ReactDOM.createRoot(htmlRoot);
+
+const reportOn = false;
 
 reactRoot.render(
   <StrictMode>
@@ -45,10 +47,10 @@ reactRoot.render(
   </StrictMode>
 );
 
-if (process.env.REACT_APP_ENV !== "production") {
+if (process.env.REACT_APP_ENV !== "production" && reportOn) {
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
   // eslint-disable-next-line no-console
-  // reportWebVitals(console.log);
+  reportWebVitals(console.log);
 }
