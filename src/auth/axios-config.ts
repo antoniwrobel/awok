7cb90d9593575ee17bce8f1151c57dd016b7235b
antoni.wrobel <antoni.wrobel@imaginecurve.com>
2023-03-02 14:16:39 +0100

    api url change

--- src/auth/axios-config.ts
+++ src/auth/axios-config.ts
@@ -73,11 +73,9 @@ const withErrorHandling = <T>(axiosInstance: AxiosInstance): AxiosInstance => {
   return axiosInstance;
 };
 
-const API_BASE_URL = "http://127.0.0.1:8000/";
-
 const axiosInstance = withErrorHandling(
   axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_URL,
     timeout: 5000,
   })
 );
